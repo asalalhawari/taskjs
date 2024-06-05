427e16d103e7614021b598f25b9bbdd44f88b6e2
ASAL HAWARI <ASALHAWARI2000@gmail.com>
2024-06-05 10:23:10 +0300

    asal

---
+++ index.js
@@ -0,0 +1,15 @@
+let cash = 1000, current_liabilities = 500;
+ console.log(cash / current_liabilities);
+////////////////////////////////////////////////
+ let revenues = 1000, expenses = 500;
+ console.log(revenues - expenses);
+/////////////////////////////////////////////
+ let liabilities = 1000, equity = 500;
+ console.log(liabilities + equity);
+////////////////////////////////////////
+ let profit = 1000, sales = 500;
+ console.log(profit * sales)
+/////////////////////////////////
+ let c = 7, o = 9, k = 2;
+ console.log((c+o+k)/3)
+////////////////////////////////
