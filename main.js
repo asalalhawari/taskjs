c77f8199ed86ca4a60594b062bfc98873186cafb
ASAL HAWARI <ASALHAWARI2000@gmail.com>
2024-06-25 20:09:54 +0300

    taskjs

---
+++ main.js
@@ -0,0 +1,45 @@
+//day1..task1
+let cash = 1000, current_liabilities = 500;
+ console.log(cash / current_liabilities);
+////////////////////////////////////////////////
+ let revenues = 1000, expenses = 500;
+ console.log(revenues - expenses);
+/////////////////////////////////////////////
+ let liabilities = 1000, equity = 500;
+ console.log(liabilities + equity);
+////////////////////////////////////////
+ let profit = 1000, sales = 500;
+ console.log(profit * sales)
+/////////////////////////////////
+ let c = 7, o = 9, k = 2;
+ console.log((c+o+k)/3)
+////////////////////////////////
+//task2
+console.log(typeof (100));      //هون بدو ياني اكتب كل وحده type تبعها عشان يطلع يا num +string...etc
+console.log(typeof (73.9));
+ console.log(typeof (NaN));
+ console.log(typeof ("Water"));
+ console.log(typeof (false));
+ console.log(typeof (9 != 11));
+ console.log("Orang" + "e");
+ console.log("Orange" - "s");
+ console.log("4" + "8");
+ console.log("4" - "8");
+ console.log("name" + 3);
+ console.log("name" - 3);
+ console.log(82 * "word");
+ console.log(1 + "hello");
+ console.log("hello" + 1);
+ console.log(1 + true);
+ console.log("hello" + true);
+ console.log(1 == '1');
+ console.log(1 === '1'); 
+
+ //task 3 بدو ياني اكتب رسالة ترحيبية 
+ //task4
+ let js= "asal";
+let jjs = "alhawari";
+let jss = "hellow world";
+console.log(js, jjs, jss);
+
+ 
